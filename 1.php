3289363f88f65e28b763ba477d27fb0bfc11cbfd
Jarin Shikalgar <97680178+jarin2810@users.noreply.github.com>
2023-12-09 13:04:08 +0530

    Add files via upload

---
+++ 1.php
@@ -0,0 +1,154 @@
+,'$whatsapp_no','$detailed_address','$outlet_address','$dealership_type','$dateoutlet','$Aadhar_Card_No',
+			'$Pan_Card_No','$gstin_No','$namefirm','$doc','$img1','$img2','$img3'
+
+
+
+
+            
+
+<p>
+				<label for="whatsappNo">Whatsapp No:</label>
+				<input type="text" name="whatsapp_no" id="whatsappno">
+			</p>
+
+<p>
+				<label for="detailedAddress">Detailed Address:</label>
+				<input type="text" name="detailed_address" id="detailedaddress">
+			</p>
+
+
+<p>
+				<label for="outletAddress">Outlet Address:</label>
+				<input type="text" name="outlet_Address" id="outletaddress">
+			</p>
+			
+	
+<p>
+				<label for="dealershiptype">Dealership Type:</label>
+				<select name = "dealership_type" id = "dealershiptype">
+				<option value = "Type1">Type 1</option>
+				<option value = "Type2">Type 2</option>
+				<option value = "Type3">Type 3</option>
+				</select>
+			</p>
+
+<p>				
+				<label for="dateoutlet">Date of Starting Outlet:</label>
+				<select name = "month" id = "month">
+				<option value = "jan">January</option>
+				<option value = "feb">February</option>
+				<option value = "mar">March</option>
+				<option value = "apr">April</option>
+				<option value = "may">May</option>
+				<option value = "jun">June</option>
+				<option value = "jul">July</option>
+				<option value = "aug">August</option>
+				<option value = "sep">September</option>
+				<option value = "oct">October</option>
+				<option value = "nov">November</option>
+				<option value = "dec">December</option>
+				</select>
+
+				<select name = "days" id = "days">
+				<option value = "1">1</option>
+				<option value = "2">2</option>
+				<option value = "3">3</option>
+				<option value = "4">4</option>
+				<option value = "5">5</option>
+				<option value = "6">6</option>
+				<option value = "7">7</option>
+				<option value = "8">8</option>
+				<option value = "9">9</option>
+				<option value = "10">10</option>
+				<option value = "11">11</option>
+				<option value = "12">12</option>
+				<option value = "13">13</option>
+				<option value = "14">14</option>
+				<option value = "15">15</option>
+				<option value = "16">16</option>
+				<option value = "17">17</option>
+				<option value = "18">18</option>
+				<option value = "19">19</option>
+				<option value = "20">20</option>
+				<option value = "21">21</option>
+				<option value = "22">22</option>
+				<option value = "23">23</option>
+				<option value = "24">24</option>
+				<option value = "25">25</option>
+				<option value = "26">26</option>
+				<option value = "27">27</option>
+				<option value = "28">28</option>
+				<option value = "29">29</option>
+				<option value = "30">30</option>
+				<option value = "31">31</option>
+				</select>
+
+				<select name = "year" id = "year">
+				<option value = "1">2001</option>
+				<option value = "2">2002</option>
+				<option value = "3">2003</option>
+				<option value = "4">2004</option>
+				<option value = "5">2005</option>
+				<option value = "6">2006</option>
+				<option value = "7">2007</option>
+				<option value = "8">2008</option>
+				<option value = "9">2009</option>
+				<option value = "10">2010</option>
+				<option value = "11">2011</option>
+				<option value = "12">2012</option>
+				<option value = "13">2013</option>
+				<option value = "14">2014</option>
+				<option value = "15">2015</option>
+				<option value = "16">2016</option>
+				<option value = "17">2017</option>
+				<option value = "18">2018</option>
+				<option value = "19">2019</option>
+				<option value = "20">2020</option>
+				<option value = "21">2021</option>
+				<option value = "22">2022</option>
+				</select>
+		</p>
+
+<p>
+				<label for="aadharcardno.">Aadhar Card No:</label>
+				<input type="text" name="Aadhar_Card_No" id="AadharCardNo.">
+			</p>
+			
+<p>
+				<label for="pancardno.">Pan Card No:</label>
+				<input type="text" name="Pan_Card_No." id="PanCardNo.">
+			</p>
+
+<p>
+				<label for="gstno.">GSTIN No:</label>
+				<input type="text" name="gstin_No." id="Gstinno.">
+			</p>
+
+<p>
+				<label for="namefirm">Name of Firm:</label>
+				<input type="text" name="namefirm" id="namefirm">
+			</p>
+
+<p>
+				<label for="dname">Document:</label>
+				<select name = "doc" id = "doc">
+				<option value = "Aadhar">Aadhar Card</option>
+				<option value = "Pan">PAN Card</option>
+				<option value = "GSTINC">GSTIN Certificate</option>
+				<option value = "Shopl">Shop Act Licence</option>
+				<option value = "rent">Rent Agreement(if any)</option>
+				<option value = "propertydoc">Property Document(if owned)</option>
+				</select><br><br>
+				<input Type = "File" name = "file" required/>
+			</p>
+
+
+<p>
+				<label for="shopimg">Upload- 3 Photos of Shop( Front Photo, Photo from Road, Inside Photo ):</label><br>
+                <input type="file" name="img1" required/><br><br>
+				<input type="file" name="img2" required/><br><br>
+				<input type="file" name="img3" required/><br>
+			</p>
+
+
+            (first_name,middle_name,last_name,mobile_number,whatsapp_number,detailed_address,outlet_address,dealership_type) 
